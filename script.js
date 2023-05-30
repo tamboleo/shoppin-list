72bd4c425f6f6bde9d5c76aeb5691dd0fbfb6fb7
Alejandro Tamboleo <atamboleogg@outlook.es>
2023-05-30 18:47:41 +0200

    Added the rest of functionalities.

--- script.js
+++ script.js
@@ -14,8 +14,23 @@ function displayItems() {
 }
 
 function onaddItemSubmit(e){
-    const newitem = itemInput.value;
     e.preventDefault();
+    const newitem = itemInput.value;
+    if (isEditMode) {
+        const itemtoEdit = itemList.querySelector('.edit-mode');
+        removeItemFromStorage(itemtoEdit.textContent);
+        itemtoEdit.classList.remove('edit-mode');
+        itemtoEdit.remove();
+        isEditMode = false;
+    }
+    else{
+        if (checkIfItemExists(newitem)) {
+            alert('Ya esta en la compraa!');
+            return
+        }
+    }
+    
+    
     if (newitem === '') {
         alert('Please add an item');
         return
@@ -71,10 +86,21 @@ function onClickItem(e) {
     setItemToEdit(e.target);
      }
 }
+
+function checkIfItemExists(item) {
+    const itemsFromStorage =getItemfromStorage();
+    return itemsFromStorage.includes(item)
+}
+
 function setItemToEdit(item) {
+    itemList.querySelectorAll('li').forEach((i) => {
+        i.classList.remove('edit-mode');
+    })
     isEditMode = true;
     item.classList.add('edit-mode');
-    formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
+    formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Actualizar';
+    formBtn.style.backgroundColor = '#228B22';
+    itemInput.value = item.textContent;
 }
 function removeItem(item) {
     if (confirm('Are you sure?')) {
@@ -99,6 +125,7 @@ function clearItems() {
     checkUI();
 }
 function checkUI() {
+    itemInput.value = '';
     const items = itemList.querySelectorAll('li');
     if (items.length === 0) {
         clearBtn.style.display = 'none';
@@ -107,6 +134,9 @@ function checkUI() {
         clearBtn.style.display = 'block';
         itemFilter.style.display = 'block';
     }
+    formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Anadir';
+    formBtn.style.backgroundColor = '#333';
+    isEditMode = false;
 }
 function filterItems(e) {
     const value = e.target.value.toLowerCase();
